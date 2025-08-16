76298f4fa29381a727189991c24d5dce3a16c179
sinclare210 <adebayooluwasegun2004@gmail.com>
2025-08-16 12:08:25 +0100

    Done

--- main.go
+++ main.go
@@ -5,19 +5,39 @@ import (
 )
 
 func main (){
-	var taxRate float64
-	var expenses float64
-	var revenue float64
+	// var taxRate float64
+	// var expenses float64
+	// var revenue float64
 
-	fmt.Print("What is your tax rate: ")
-	fmt.Scan(&taxRate)
 
-	fmt.Print("What are your expenses: ")
-	fmt.Scan(&expenses)
+	// fmt.Print("What are your expenses: ")
+	// fmt.Scan(&expenses)
+	expenses := userInput("What are your expenses: ")
 
-	fmt.Print("What is your revenue: ")
-	fmt.Scan(&revenue)
+	// fmt.Print("What is your revenue: ")
+	// fmt.Scan(&revenue)
 
+	revenue := userInput("What are your revenue: ")
+
+	taxRate := userInput("What is your taxt rate: ")
+
+	earningBeforeTax,earningAfterTax,profitRatio := result(expenses,revenue,taxRate)
+
+	fmt.Println("Your Earning Before Tax is ", earningBeforeTax)
+
+	fmt.Println("Your Earning After Tax is ", earningAfterTax)
+
+	fmt.Println("Your Profit Ratio is ", profitRatio)
+}
+
+func userInput (input string) float64{
+	var userInput float64
+	fmt.Print(input)
+	fmt.Scan(&userInput)
+	return  userInput
+}
+
+func result (expenses,revenue,taxRate float64)(float64, float64, float64){
 	earningBeforeTax := revenue - expenses
 
 	taxAmount := earningBeforeTax * taxRate
@@ -26,9 +46,5 @@ func main (){
 
 	profitRatio := (earningAfterTax/revenue) * 100
 
-	fmt.Println("Your Earning Before Tax is ", earningBeforeTax)
-
-	fmt.Println("Your Earning After Tax is ", earningAfterTax)
-
-	fmt.Println("Your Profit Ratio is ", profitRatio)
+	return earningBeforeTax,earningAfterTax,profitRatio
 }
